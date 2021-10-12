856134407ca18ac9af2b54e74cf5d96952501113
Kelvin Arnold <kelvin.arnold.0030@gmail.com>
2021-10-12 18:30:44 -0300

    TASK-39: Badge component v1

--- src/ui/Text/Text.styled.tsx
+++ src/ui/Text/Text.styled.tsx
@@ -1,12 +1,8 @@
 // Component generated with util/create-component.js
 import styled from "styled-components";
 import classnames from "classnames";
-import {Preset, Color} from "./Text.types";
+import {TextProps} from "./Text.types";
 
-type TextProps = {
-	preset: keyof typeof Preset;
-	color: keyof typeof Color;
-};
 /**
  * Components
  */
@@ -14,6 +10,7 @@ export const TextWrapper = styled.span.attrs<TextProps>(({preset, color}) => {
 	const className = classnames({
 		prose: preset === "BODY", // 16px
 		"prose prose-sm": preset === "BODY_02", // 14px
+		"prose prose-xs": preset === "BODY_03", // 12px
 		"prose font-bold": preset === "BUTTON" || preset === "SUBTITLE", // 16px
 		"prose prose-sm font-bold": preset === "BUTTON_02", // 14px
 		"prose prose-4xl font-bold": preset === "HEADLINE_01", // 60px
@@ -22,6 +19,7 @@ export const TextWrapper = styled.span.attrs<TextProps>(({preset, color}) => {
 		"prose prose-xl font-bold": preset === "HEADLINE", // 24px
 		"prose prose-2lg font-bold": preset === "HEADLINE_05", // 20px
 		"prose prose-lg font-bold": preset === "HEADLINE_06", // 18px
+		"text-current": color === "INHERIT",
 		"text-primary": color === "PRIMARY",
 		"text-secondary": color === "SECONDARY",
 		"text-white": color === "WHITE",

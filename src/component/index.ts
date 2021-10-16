ef5852a1918f112813fa5271080c9f52d704b57f
hada <dajung.cv@gmail.com>
2021-10-16 20:53:22 -0300

    TASK-62: export radio table as component

--- src/component/index.ts
+++ src/component/index.ts
@@ -3,3 +3,4 @@ export {default as CPNSelectInput} from "./SelectInput/SelectInput";
 export {default as CPNAlert} from "./Alert/Alert";
 export {default as CPNRadioButton} from "./RadioButton/RadioButton";
 export {default as CPNCheckBox} from "./CheckBox/CheckBox";
+export {default as CPNRadioTable} from "./RadioTable/RadioTable";

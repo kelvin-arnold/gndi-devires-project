5e53792d66679f886b9419649ef3102aff96d096
hada <dajung.cv@gmail.com>
2021-10-18 17:16:25 -0300

    TASK-48: update textarea component

--- src/component/Textarea/Textarea.tsx
+++ src/component/Textarea/Textarea.tsx
@@ -1,31 +1,24 @@
 // Component generated with util/vox-create-component.js
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {UIText} from "../../ui";
 import {TextareaWrapper, TextareaInput} from "./Textarea.styled";
 import {TextareaProps} from "./Textarea.types";
 
 export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-	({label, maxLength, ...args}, inRef) => {
-		// Refs
-		const ref = React.useRef<HTMLTextAreaElement>(null);
-		React.useImperativeHandle(inRef, () => ref.current!, [ref]);
+	({label, maxLength, ...args}, ref) => {
 		// Context Here
 		// States Here
 		const [length, setLength] = useState<number>(0);
 		// Effects Here
 		// Handlers Here
 		return (
-			<TextareaWrapper {...args}>
+			<TextareaWrapper>
 				{label && (
 					<UIText preset="BODY_02" color="GREY">
 						{label}
 					</UIText>
 				)}
-				<TextareaInput
-					{...{maxLength}}
-					ref={ref}
-					onChange={(e) => setLength(e.target.value.length)}
-				/>
+				<TextareaInput {...args} ref={ref} onChange={(e) => setLength(e.target.value.length)} />
 				{maxLength && (
 					<div className="text-right">
 						<UIText preset="BODY_02" color="GREY">

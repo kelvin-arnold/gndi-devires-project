f592c0cc74145074cdf1d6ecbe3aef873a296a95
Kelvin Arnold <kelvin.arnold.0030@gmail.com>
2021-11-13 12:48:57 -0300

    TASK-75: Componente Chart

--- src/component/Chart/Chart.test.tsx
+++ src/component/Chart/Chart.test.tsx
@@ -7,7 +7,14 @@ const CHART_TEST_ID = "CHART_TEST_ID";
 
 describe("Chart component", () => {
 	it("should render component", async () => {
-		render(<Chart data-testid={CHART_TEST_ID} />);
+		render(
+			<Chart
+				title="80"
+				values={[20, 20, 60]}
+				labels={["Label 1", "Label 2", "Label 3"]}
+				data-testid={CHART_TEST_ID}
+			/>,
+		);
 		const renderComponent = await waitFor(() => screen.getByTestId(CHART_TEST_ID));
 		expect(renderComponent).toBeInTheDocument();
 	});
